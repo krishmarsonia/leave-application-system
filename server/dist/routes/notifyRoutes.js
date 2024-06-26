e7b338a113e8e1ecfee387f5b95a289ae5d8477d
krishmarsonia <krishmarsonia@gmail.com>
2024-06-26 16:51:31 +0530

    removed dist directory

--- server/dist/routes/notifyRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const notificationController_1 = require("../controllers/notificationController");
-const router = (0, express_1.Router)();
-router.get("/getNotifications/:userId", notificationController_1.getNotificationsController);
-router.put("/setNotifications/:userId", notificationController_1.setNotificationsController);
-exports.default = router;
